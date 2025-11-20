0012273681bcd0b3e9476796e678a7007b0089a8
kent011011 <kent.edulzura@urios.edu.ph>
2025-11-20 15:06:03 +0800

    ragassfinal

---
+++ d
@@ -0,0 +1,10 @@
+  SearchAndView[m
+  StudFacultyReg[m
+  ViewFaculty[m
+  archive[m
+  archiverestore[m
+  goodmorning[m
+  main[m
+* [32mragasfinal[m
+  system-settings[m
+  updepcourse[m
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ragas and Edulzura</title>
+    <title>Ragas</title>
     @viteReactRefresh
     @vite('resources/js/app.js')
 </head>
